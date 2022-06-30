7bc253078ec501c7927dc58e72ad477ad2160b15
RFUERTES\Rfuertes-LP <reynelfuertes@gmail.com>
2022-07-01 02:24:50 +0800

    add topnav search

--- src/app/shared/components/topnav/topnav.component.ts
+++ src/app/shared/components/topnav/topnav.component.ts
@@ -1,5 +1,5 @@
 import { environment } from './../../../../environments/environment';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { GenericDestroyPageComponent } from '../../generics/destroy.generic';
@@ -12,9 +12,19 @@ import { RootState } from 'src/app/store/root.reducer';
 })
 
 export class TopNavComponent extends GenericDestroyPageComponent {
+  @ViewChild('search') searchElement: ElementRef;
+
   public imgPath: string = environment.imgPath;
-  
+  public showAppSearch: boolean = false;
+
   constructor(private router: Router, private store: Store<RootState>) {
     super();
+  }
+
+  public showSearch(): void {
+    this.showAppSearch = !this.showAppSearch;
+    setTimeout(() => {
+      this.searchElement.nativeElement.focus();
+    });
   }
 }
