550428a23080d8072463fbe6342247bead9d5c05
RFUERTES\Rfuertes-LP <reynelfuertes@gmail.com>
2022-07-01 03:02:30 +0800

    add sidebar toggle

--- src/app/shared/components/topnav/topnav.component.ts
+++ src/app/shared/components/topnav/topnav.component.ts
@@ -1,5 +1,5 @@
 import { environment } from './../../../../environments/environment';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { GenericDestroyPageComponent } from '../../generics/destroy.generic';
@@ -13,12 +13,19 @@ import { RootState } from 'src/app/store/root.reducer';
 
 export class TopNavComponent extends GenericDestroyPageComponent {
   @ViewChild('search') searchElement: ElementRef;
+  @Output() valueEmitter: EventEmitter<boolean> = new EventEmitter();
 
   public imgPath: string = environment.imgPath;
   public showAppSearch: boolean = false;
+  public toggleSideNav: boolean = true;
 
   constructor(private router: Router, private store: Store<RootState>) {
     super();
+  }
+
+  public onToggleSideNav(): void {
+    this.toggleSideNav = !this.toggleSideNav;
+    this.valueEmitter.emit(this.toggleSideNav);
   }
 
   public showSearch(): void {
